9dc1eefa934bb4f040d8ba3329dbc6b3b425b349
saylani-massit-hyderbad <saylanimasshyderabad@gmail.com>
2022-01-22 13:19:28 +0500

    added files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import Head from './Header'
+
+function Data() {
+  return (
+    <div style={{color:"yellow"}}>
+      <h1 className="Header">Hello World</h1>
+      <h2>Smit Hyderabad</h2>
+    <Head/>
+    
+    </div>
+  )
+}
+
+
+export default Data
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function Header() {
+  return (
+    <div style={{color:"yellow"}}>
+      <h1 className="Header">Header</h1>
+     
+      <Header/>
+    
+    </div>
+  )
+}
+
+
+export default Header
